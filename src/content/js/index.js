1920faceb8be721a092c783590551622fe66c311
Abhishek Kumar <abhishekkumarjnvk@gmail.com>
2021-05-08 15:09:48 +0530

    ranking content

--- src/content/js/index.js
+++ src/content/js/index.js
@@ -4,7 +4,12 @@ const moment = require("moment");
 const os = require("os");
 const md5 = require("md5");
 const jsStringEscape = require("js-string-escape");
-var exec = require("child_process").exec;
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+const shell = require("electron").shell;
+const path = require("path");
+
 var files = [];
 var current_directory = os.homedir();
 var current_location = current_directory + "/Documents";
@@ -30,9 +35,10 @@ function loadSideBar(directory, div_id = "sidebar_list") {
 function addIntoSideBar(directory, file, div_id) {
   var id = md5(directory + "/" + file);
   $(`#${div_id}`).append(
-    `<li class="p-2 sidebar_folder border" location="${directory + "/" + file}">
-      ${file}
-      <div id="child_${id}"></div>
+    `<li class="p-2 py-3 mt-1 sidebar_folder border" location="${
+      directory + "/" + file
+    }">
+      <i class="fas fa-folder"></i> ${file}
     </li>`
   );
 }
@@ -51,9 +57,8 @@ $(document).ready(function () {
 function LoadAllFiles() {
   $("#table_content").empty();
   if (fs.existsSync(current_location)) {
-    files = fs.readdirSync(current_location);
+    files = getFiles(current_location);
     files.forEach((file) => {
-      // console.log(file);
       addEntry(current_location + "/" + file);
     });
   } else {
@@ -63,34 +68,46 @@ function LoadAllFiles() {
 
 function addEntry(location) {
   let stats = fs.statSync(location);
+  if (stats.isDirectory()) {
+    var file_class = "fas fa-folder";
+  } else {
+    file_class = "far fa-file-alt";
+  }
   let name = location.split("/");
   name = name[name.length - 1];
-  let updateString = `<tr><td>${name}</td><td>${
+  let updateString = `<tr><td><i class="${file_class}"></i> ${name}</td><td><small>${
     stats.size / 1000
-  } KB</td><td>${moment(stats.mtime).format("Do MMMM, h:mm A")}</td></tr>`;
+  } </small></td><td><small>${moment(stats.mtime).format(
+    "Do MMMM, h:mm A"
+  )}</small></td><td><button class="btn btn-default" onclick="openFile('${location}')"><i class="fas fa-external-link-alt"></i></button></td></tr>`;
   $("#table_content").append(updateString);
 }
 
 function search(term) {
   $("#table_content").empty();
-  var command = `grep "${term}" ${current_location}/* -i -l -R`;
-  // console.log(command);
-  exec(command, function (error, stdout, stderr) {
+  let files = getFiles(current_location);
+  let searched_file = files.map(async (element) => {
+    var command = `grep "${term}" "${current_location}/${element}" -i -R -o | wc -l`;
+    const { stdout, stderr } = await exec(command);
+    if (stdout > 0) {
+      return [element, stdout.trim()];
+    }
     if (stderr) {
       console.log(stderr);
-    } else {
-      // console.log(stdout);
-      var all_file = stdout.trim().split("\n");
-      all_file.forEach((single) => {
-        single = single.split(":");
-        var file_location = single[0].trim();
-        if (file_location) addEntry(file_location);
-      });
-      if (all_file.length === 1 && !all_file[0]) {
-        let updateString = `<tr><td  colspan="3">No File Found</td></tr>`;
-        $("#table_content").append(updateString);
-      }
     }
+  });
+  Promise.all(searched_file).then(function (results) {
+    results = results.filter(function (element) {
+      return element !== undefined;
+    });
+    let ranked_files = results.sort(function (a, b) {
+      return b[1] - a[1];
+    });
+    ranked_files.map((single_file) => {
+      console.log("Occurrence is " + single_file[1] + " Times");
+      var file_location = single_file[0].trim();
+      if (file_location) addEntry(current_location + "/" + file_location);
+    });
   });
 }
 
@@ -101,5 +118,18 @@ $("#search_term").on("input", function () {
   else LoadAllFiles();
 });
 
+function getFiles(location) {
+  let files = fs.readdirSync(location);
+  files = files.filter((file) => {
+    let stats = fs.statSync(location + "/" + file);
+    if (!stats.isDirectory()) return file;
+  });
+  return files;
+}
+
+function openFile(file_location) {
+  if (confirm("Launching in a external application"))
+    shell.openPath(file_location);
+}
 LoadAllFiles();
 loadSideBar(current_directory);
